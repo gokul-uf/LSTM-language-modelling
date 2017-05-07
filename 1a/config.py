59f9cbd32759b49b269a939d5955bc0dd7016c84
gokul <gokul.uf@gmail.com>
2017-05-07 22:21:57 +0200

    num_hidden_state set to 512 instead of 1024

--- 1a/config.py
+++ 1a/config.py
@@ -2,7 +2,7 @@ class Config:
     batch_size = 64
     embed_size = 100
     vocab_size = 20000
-    num_hidden_state = 1024 # change to 1024 for task 1c
+    num_hidden_state = 512 # change to 1024 for task 1c
     proj_hidden_state = 512 # only for task 1c
     seq_length = 30
     top_words = 20000 - 4
@@ -10,7 +10,7 @@ class Config:
     num_epochs = 20
     pkl_dir = "pkls"
     ckpt_dir = "checkpoints/"
-    mode = "TRAIN" # could be "TRAIN" or "TEST"
-    ckpt_file = ""
+    mode = "TEST" # could be "TRAIN" or "TEST"
+    ckpt_file = "epoch_5.ckpt"
     word2vec_path = "../wordembeddings-dim100.word2vec"
     test_file = "../data/sentences_test"
